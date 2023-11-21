562d4c8f8d415f383c65cf3ac50167ea40d7c28d
nazarisabbot <n.updesign@gmail.com>
2023-11-21 21:12:56 +0600

    add some png

--- js/index.js
+++ js/index.js
@@ -1704,7 +1704,7 @@ const callModalWindow = (lang, res, flag) => {
   divCross.appendChild(span1);
   divCross.appendChild(span2);
   const img = document.createElement('img');
-  const path = res === 'ok' ? './img/true.svg' : './img/false.svg';
+  const path = res === 'ok' ? './img/true.svg' : './img/false.png';
   img.setAttribute('src', path);
   divBody.appendChild(img);
   const p = document.createElement('p');
